a9ba2404f50ae4e39fa1f745f292bb283673100a
Borges Diaz <devborgesdiaz@gmail.com>
2016-06-30 14:16:29 -0400

    Misc...

--- app/Http/Controllers/Pages/Main/HomeController.php
+++ app/Http/Controllers/Pages/Main/HomeController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Pages\Main;
 
 use App\Http\Controllers\Pages\Main\MainPageController;
 use View;
+use Response;
+use JWTAuth;
+use Hash;
+use App\User;
 
 class HomeController extends MainPageController
 {
     
     public function index() {
+      
         
+        /*ob_start();
+        var_dump($user);
+        $res = ob_get_clean();
+        file_put_contents('newfile.txt', $res);*/
         return View::make('main.index');
     }
 }
